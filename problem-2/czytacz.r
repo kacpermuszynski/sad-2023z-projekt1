6ec98a229dac8151832c790ea753a22c8c3ad31f
Daniel <daniel.gorniak@op.pl>
2023-12-09 23:59:35 +0100

    my proposition

--- problem-2/czytacz.r
+++ problem-2/czytacz.r
@@ -7,15 +7,15 @@ LiczbaLiczb = fsize / BajtowNaLiczbe
 v<-readBin(zz, numeric(), size=BajtowNaLiczbe, endian="little", n=LiczbaLiczb)
 close(zz)
 
+# Ustawienie progu detekcji impulsów na 0.9998 kwantyla
+threshold = quantile(v, 0.99985)
+
 print(v[1:10])
 plot(v[1:100000], type='l')     # dłuższy fragment
 idx = 67650:67900;
-plot(idx, v[idx],type='l')      # jeden impuls - aby zobaczyć kształt impulsów
+plot(idx, v[idx],type='l')# jeden impuls - aby zobaczyć kształt impulsów
 
 # Zadanie 2
-
-# Ustawienie progu detekcji impulsów na 90% kwantyla
-threshold = quantile(v, 0.90)
 
 print(threshold)
 
@@ -25,10 +25,18 @@ impulse_indices <- which(v > threshold)
 # Znajdź indeksy, gdzie sygnał nie przekracza progu (indeksy nie-impulsów)
 non_impulse_indices <- setdiff(1:length(v), impulse_indices)
 
-# Wykres sygnału z zaznaczonymi impulsami
-plot(v, type = 'l', main = "Sygnał z wykrytymi impulsami")
-points(non_impulse_indices, v[non_impulse_indices], col = 'blue', pch = 16)
+# Wykres części sygnału z zaznaczonymi impulsami
+idx_max = length(v)/20
+v_part = v[0 : idx_max]
+impulse_indices_part = impulse_indices[impulse_indices <= idx_max]
+plot(v_part, type = 'l', main = "Sygnał z wykrytymi impulsami")
+points(impulse_indices_part, v[impulse_indices_part], col = 'blue', pch = 16)
 
+idx = 67650:67900;
+plot(idx, v[idx],type='l')# jeden impuls - aby zobaczyć kształt impulsów
+impulse_indices2 = impulse_indices[impulse_indices >= 67650]
+impulse_indices2 = impulse_indices2[impulse_indices2 <= 67900]
+points(impulse_indices2, v[impulse_indices2], col = 'blue', pch = 16)
 
 #a)
 # Analiza fragmentu sygnału bez impulsów
@@ -36,27 +44,25 @@ non_impulse_signal <- v[non_impulse_indices]
 
 # Wykres histogramu
 hist(non_impulse_signal, main="Histogram Sygnału bez Impulsów", xlab="Wartość", ylab="Liczebność", col="lightblue")
+boxplot(non_impulse_signal, main="Wykres pudełkowy Sygnału bez Impulsów", xlab="Wartość", ylab="Liczebność", col="lightblue")
 
 #b)Badanie rozkładu odstępów czasu między impulsami
 
 # Znajdź odstępy czasu między impulsami
-time_gaps <- diff(impulse_indices)
+ilosc_probek_miedzy_impulsami <- diff(impulse_indices)
 
-# Przelicz odstępy czasu na ilość próbek
-ilosc_probek_miedzy_impulsami <- time_gaps * 50e6  # Przeliczamy na ilość próbek, gdzie 50e6 to częstotliwość próbkowania 50 MHz
-print(ilosc_probek_miedzy_impulsami)
-
-# Wybierz tylko te odstępy czasu, które są wystarczająco krótkie
-short_time_gaps <- ilosc_probek_miedzy_impulsami[ilosc_probek_miedzy_impulsami < 1000000]
-print(short_time_gaps)
+# Wybierz tylko te odstępy czasu, które są wystarczająco długie
+time_gaps <- ilosc_probek_miedzy_impulsami[ilosc_probek_miedzy_impulsami > 200]
+print(time_gaps)
 
 
-# Sprawdź, czy short_time_gaps nie jest puste
-if (length(short_time_gaps) > 0) {
+# Sprawdź, czy time_gaps nie jest puste
+if (length(time_gaps) > 0) {
   num_breaks <- 30
 
   # Wykres histogramu odstępów czasu
-  hist(short_time_gaps, breaks = num_breaks, main = "Histogram Krótkich Odstępów Czasu Między Impulsami", xlab = "Odstęp czasu", ylab = "Liczebność", col = "lightgreen")
+  time_gaps = time_gaps  / 50e6 * 1000 # w ms
+  hist(time_gaps, breaks = num_breaks, main = "Histogram Odstępów Czasu Między Impulsami", xlab = "Odstęp czasu [ms]", ylab = "Liczebność", col = "lightgreen")
 } else {
   cat("Brak danych do wygenerowania histogramu.\n")
 }
