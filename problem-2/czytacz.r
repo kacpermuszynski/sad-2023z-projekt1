cf85f7f9317dc2b0318df7d1c3062a0a0bcbc399
kacpermuszynski <kacper.muszynski@yahoo.com>
2023-12-09 23:47:58 +0100

    Problem-2 improvements

--- problem-2/czytacz.r
+++ problem-2/czytacz.r
@@ -8,14 +8,19 @@ v<-readBin(zz, numeric(), size=BajtowNaLiczbe, endian="little", n=LiczbaLiczb)
 close(zz)
 
 print(v[1:10])
-plot(v[1:100000], type='l')     # dłuższy fragment
+png("problem-2/wykresy/dluzszy_fragment_sygnalu.png")
+plot(v[1:100000], type='l', main='Dluzszy fragment sygnalu', xlab='Przedzial', ylab='Wartosc natezenia') # dluzszy fragment sygnalu
+dev.off()
+
+png("problem-2/wykresy/pojedynczy_impuls.png")
 idx = 67650:67900;
-plot(idx, v[idx],type='l')      # jeden impuls - aby zobaczyć kształt impulsów
+plot(idx, v[idx],type='l', main='Pojedynczy impuls', xlab='Przedzial', ylab='Wartosc natezenia')      # jeden impuls - aby zobaczyć kształt impulsów
+dev.off()
 
 # Zadanie 2
 
-# Ustawienie progu detekcji impulsów na 90% kwantyla
-threshold = quantile(v, 0.90)
+# Ustawienie progu detekcji impulsów na 95% kwantyla
+threshold = quantile(v, 0.95)
 
 print(threshold)
 
@@ -25,39 +30,26 @@ impulse_indices <- which(v > threshold)
 # Znajdź indeksy, gdzie sygnał nie przekracza progu (indeksy nie-impulsów)
 non_impulse_indices <- setdiff(1:length(v), impulse_indices)
 
-# Wykres sygnału z zaznaczonymi impulsami
-plot(v, type = 'l', main = "Sygnał z wykrytymi impulsami")
-points(non_impulse_indices, v[non_impulse_indices], col = 'blue', pch = 16)
-
-
 #a)
 # Analiza fragmentu sygnału bez impulsów
 non_impulse_signal <- v[non_impulse_indices]
 
 # Wykres histogramu
-hist(non_impulse_signal, main="Histogram Sygnału bez Impulsów", xlab="Wartość", ylab="Liczebność", col="lightblue")
+png("problem-2/wykresy/histogram_szumu.png")
+hist(non_impulse_signal, main="Histogram sygnału bez impulsow", xlab="Natezenie sygnalu", ylab="Liczebnosc", col="lightblue")
+dev.off()
 
 #b)Badanie rozkładu odstępów czasu między impulsami
 
 # Znajdź odstępy czasu między impulsami
 time_gaps <- diff(impulse_indices)
 
+# Zatrzymaj wieksze od 100, czyli roznica miedzy koncem jednego a poczatkiem drugiego impulsu
+time_gaps_filtered <- time_gaps[time_gaps > 100]
+
 # Przelicz odstępy czasu na ilość próbek
-ilosc_probek_miedzy_impulsami <- time_gaps * 50e6  # Przeliczamy na ilość próbek, gdzie 50e6 to częstotliwość próbkowania 50 MHz
-print(ilosc_probek_miedzy_impulsami)
+ilosc_probek_miedzy_impulsami <- time_gaps_filtered / 50e6  # Przeliczamy na ilość próbek, gdzie 50e6 to częstotliwość próbkowania 50 MHz
 
-# Wybierz tylko te odstępy czasu, które są wystarczająco krótkie
-short_time_gaps <- ilosc_probek_miedzy_impulsami[ilosc_probek_miedzy_impulsami < 1000000]
-print(short_time_gaps)
-
-
-# Sprawdź, czy short_time_gaps nie jest puste
-if (length(short_time_gaps) > 0) {
-  num_breaks <- 30
-
-  # Wykres histogramu odstępów czasu
-  hist(short_time_gaps, breaks = num_breaks, main = "Histogram Krótkich Odstępów Czasu Między Impulsami", xlab = "Odstęp czasu", ylab = "Liczebność", col = "lightgreen")
-} else {
-  cat("Brak danych do wygenerowania histogramu.\n")
-}
-
+png("problem-2/wykresy/histogram_odstepu_czasu.png")
+hist(ilosc_probek_miedzy_impulsami, main = "Histogram odstępów czasu miedzy impulsami", xlab = "Odstep czasu", ylab = "Liczebnosc", col = "lightgreen")
+dev.off()
